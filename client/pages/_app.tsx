e3a276f1af5e8f65fcb7b8071f270e45f856faff
Srivastava57Harsh <srivastava57harsh@gmail.com>
2023-03-23 19:18:59 +0530

    :sparkles: setup dashboard
    setup dashboard

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,39 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import type { AppProps } from "next/app";
+import { mainnet, goerli } from "wagmi/chains";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Dashboard from "./dashboard";
+
+const { chains, provider } = configureChains(
+  [mainnet, goerli],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Hashstack",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Route path="/dashboard">
+          <Navbar />
+          <Dashboard />
+        </Route>
+        <Component {...pageProps} />{" "}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
