375bca8b14e36776813b4c756fa509526a7ec603
Ankit Choudhary <sign.ankit14@gmail.com>
2023-03-26 20:13:29 +0530

    Create Upload_certificate.jsx

---
+++ client/components/Upload_certificate.jsx
@@ -0,0 +1,596 @@
+import { ethers } from "ethers";
+import React, { useState } from "react";
+import { Web3Storage } from "web3.storage";
+
+const CONTRACT_ADDRESS = "0xfb0dc79b7ee0Fc84a9E4FF032E6152C7ABc626Be";
+const ABI = [
+	{
+		inputs: [],
+		stateMutability: "nonpayable",
+		type: "constructor",
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: "address",
+				name: "owner",
+				type: "address",
+			},
+			{
+				indexed: true,
+				internalType: "address",
+				name: "approved",
+				type: "address",
+			},
+			{
+				indexed: true,
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "Approval",
+		type: "event",
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: "address",
+				name: "owner",
+				type: "address",
+			},
+			{
+				indexed: true,
+				internalType: "address",
+				name: "operator",
+				type: "address",
+			},
+			{
+				indexed: false,
+				internalType: "bool",
+				name: "approved",
+				type: "bool",
+			},
+		],
+		name: "ApprovalForAll",
+		type: "event",
+	},
+	{
+		inputs: [
+			{
+				internalType: "address",
+				name: "to",
+				type: "address",
+			},
+			{
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "approve",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: "address",
+				name: "to",
+				type: "address",
+			},
+			{
+				indexed: true,
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "Attest",
+		type: "event",
+	},
+	{
+		inputs: [
+			{
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "burn",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: "address",
+				name: "previousOwner",
+				type: "address",
+			},
+			{
+				indexed: true,
+				internalType: "address",
+				name: "newOwner",
+				type: "address",
+			},
+		],
+		name: "OwnershipTransferred",
+		type: "event",
+	},
+	{
+		inputs: [],
+		name: "renounceOwnership",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "revoke",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: "address",
+				name: "to",
+				type: "address",
+			},
+			{
+				indexed: true,
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "Revoke",
+		type: "event",
+	},
+	{
+		inputs: [
+			{
+				internalType: "address",
+				name: "to",
+				type: "address",
+			},
+			{
+				internalType: "string",
+				name: "uri",
+				type: "string",
+			},
+		],
+		name: "safeMint",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "address",
+				name: "from",
+				type: "address",
+			},
+			{
+				internalType: "address",
+				name: "to",
+				type: "address",
+			},
+			{
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "safeTransferFrom",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "address",
+				name: "from",
+				type: "address",
+			},
+			{
+				internalType: "address",
+				name: "to",
+				type: "address",
+			},
+			{
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+			{
+				internalType: "bytes",
+				name: "data",
+				type: "bytes",
+			},
+		],
+		name: "safeTransferFrom",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "address",
+				name: "operator",
+				type: "address",
+			},
+			{
+				internalType: "bool",
+				name: "approved",
+				type: "bool",
+			},
+		],
+		name: "setApprovalForAll",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: "address",
+				name: "from",
+				type: "address",
+			},
+			{
+				indexed: true,
+				internalType: "address",
+				name: "to",
+				type: "address",
+			},
+			{
+				indexed: true,
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "Transfer",
+		type: "event",
+	},
+	{
+		inputs: [
+			{
+				internalType: "address",
+				name: "from",
+				type: "address",
+			},
+			{
+				internalType: "address",
+				name: "to",
+				type: "address",
+			},
+			{
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "transferFrom",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "address",
+				name: "newOwner",
+				type: "address",
+			},
+		],
+		name: "transferOwnership",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "address",
+				name: "owner",
+				type: "address",
+			},
+		],
+		name: "balanceOf",
+		outputs: [
+			{
+				internalType: "uint256",
+				name: "",
+				type: "uint256",
+			},
+		],
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "getApproved",
+		outputs: [
+			{
+				internalType: "address",
+				name: "",
+				type: "address",
+			},
+		],
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "address",
+				name: "owner",
+				type: "address",
+			},
+			{
+				internalType: "address",
+				name: "operator",
+				type: "address",
+			},
+		],
+		name: "isApprovedForAll",
+		outputs: [
+			{
+				internalType: "bool",
+				name: "",
+				type: "bool",
+			},
+		],
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		inputs: [],
+		name: "name",
+		outputs: [
+			{
+				internalType: "string",
+				name: "",
+				type: "string",
+			},
+		],
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		inputs: [],
+		name: "owner",
+		outputs: [
+			{
+				internalType: "address",
+				name: "",
+				type: "address",
+			},
+		],
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "ownerOf",
+		outputs: [
+			{
+				internalType: "address",
+				name: "",
+				type: "address",
+			},
+		],
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "bytes4",
+				name: "interfaceId",
+				type: "bytes4",
+			},
+		],
+		name: "supportsInterface",
+		outputs: [
+			{
+				internalType: "bool",
+				name: "",
+				type: "bool",
+			},
+		],
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		inputs: [],
+		name: "symbol",
+		outputs: [
+			{
+				internalType: "string",
+				name: "",
+				type: "string",
+			},
+		],
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				internalType: "uint256",
+				name: "tokenId",
+				type: "uint256",
+			},
+		],
+		name: "tokenURI",
+		outputs: [
+			{
+				internalType: "string",
+				name: "",
+				type: "string",
+			},
+		],
+		stateMutability: "view",
+		type: "function",
+	},
+];
+
+
+const Upload_certificate = () => {
+	const [image, setImage] = useState();
+	const [name, setName] = useState("");
+	const [address, setAddress] = useState("");
+
+	const CheckHash = async () => {
+		try {
+			console.log("Begin");
+			const { ethereum } = window;
+
+			if (ethereum) {
+        
+				console.log("Going to pop wallet to connect");
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const connectedContract = new ethers.Contract(
+					CONTRACT_ADDRESS,
+					ABI,
+					signer
+				);
+				const accounts = await ethereum.request({
+					method: "eth_requestAccounts",
+				});
+				console.log("Connected", accounts[0]);
+
+				console.log(image);
+				console.log(name);
+
+				function getAccessToken() {
+					return process.env.REACT_APP_ACCESS_TOKEN;
+				}
+
+				function makeStorageClient() {
+					return new Web3Storage({ token: getAccessToken() });
+				}
+
+				async function makeFileObjects(questions) {
+					const files = [new File([questions], "nftInfo.json")];
+					console.log(files);
+					return files;
+				}
+
+				async function storeFiles(questions) {
+					console.log("Uploading Metadata to IPFS with web3.storage....");
+
+					const files = await makeFileObjects(questions);
+					const client = makeStorageClient();
+					const cid = await client.put(files, { wrapWithDirectory: false });
+
+					return cid;
+				}
+
+				const storeImage = async (files) => {
+					console.log("Uploading image to IPFS with web3.storage....");
+
+					const client = makeStorageClient();
+					const cid = await client.put([files], { wrapWithDirectory: false });
+
+					return cid;
+				};
+
+				console.log(image);
+				const ImageCID = await storeImage(image);
+				console.log(`${ImageCID}`);
+
+				const metadata = JSON.stringify({
+					name: `Degree Certificate of ${name}`,
+					description:
+						"Soulbound token, claimed by college students who attended XYZ University",
+					image: `ipfs://${ImageCID}`,
+				});
+
+				console.log(metadata);
+				const QuestionCID = await storeFiles(metadata);
+				console.log(`${QuestionCID}`);
+
+				let minting = await connectedContract.safeMint(
+					`${address}`,
+					`ipfs://${QuestionCID}`
+				);
+				console.log("Minting: Process started");
+				await minting.wait();
+				console.log("Minting: Process finished");
+			} else {
+				console.log("Ethereum object doesn't exist!");
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<div>
+			<div>
+				<p className="xl:w-1/5 w-1/4">Name: </p>
+				<input value={name} onChange={(e) => setName(e.target.value)} />
+			</div>
+			<div>
+				<p className="xl:w-1/5 w-1/4">Address: </p>
+				<input value={address} onChange={(e) => setAddress(e.target.value)} />
+			</div>
+			<div>
+				<p>Certificate</p>
+				<input
+					type="file"
+					className="flex-1 py-[5px] px-4 outline-none focus:bg-white/5 bg-black/25  border-white/10 border-primaryWidth rounded-lg transition"
+					accept="image/png, image/jpeg"
+					onChange={(e) => setImage(e.target.files[0])}
+				/>
+			</div>
+			<button onClick={() => CheckHash().then(/* handle promise here */)}>
+				Mint
+			</button>
+		</div>
+	);
+};
+
+export default Upload_certificate;
