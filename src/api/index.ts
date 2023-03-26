f395666101e14ae3c197c2a166b9324a6acf44ed
Srivastava57Harsh <srivastava57harsh@gmail.com>
2023-03-26 17:45:11 +0530

    add backend (retains bug)

---
+++ src/api/index.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import authRouter from "./auth/router";
+import adminAuthRouter from "./admin/router";
+import dashboardRouter from "./dashboard/router";
+import razorpayRouter from "./razorpay/router";
+import webhookRouter from "./webhook/router";
+
+export default (): Router => {
+  const app = Router();
+
+  app.use("/auth", authRouter);
+  app.use("/admin", adminAuthRouter);
+  app.use("/dashboard", dashboardRouter);
+
+  return app;
+};
